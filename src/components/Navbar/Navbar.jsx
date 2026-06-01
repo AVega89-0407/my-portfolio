c18435c531fd3c3cbae1b399d9686381913962ad
Andrea Vega <andrea.vega@live.se>
2026-06-01 17:04:16 +0200

    changed about to skills

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
       <p>andrea.dev</p></a>
         <ul className="navbar-links">
             <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#about">Skills</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
